e82717adee9759d4357b2325b10f926fedc6ca33
Brent Baker <dcgator8@hotmail.com>
2021-01-23 21:33:47 -0500

    Initial Commit. Log In and Sign Up page

---
+++ app.js
@@ -0,0 +1,38 @@
+//jshint esversion: 6
+
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/login.html");
+});
+
+app.post("/", function(req, res) {
+  res.sendFile(__dirname + "/failure.html");
+})
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
+
+app.post("/create", function(req, res) {
+  res.sendFile(__dirname + "/create.html");
+})
+
+app.post("/createsubmit", function(req, res) {
+  res.sendFile(__dirname + "/failure.html");
+})
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000.");
+});
