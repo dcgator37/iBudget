5d92d77d6077a3b95462401e3b300562e2ea7bda
Brent Baker <dcgator8@hotmail.com>
2021-01-30 10:42:01 -0500

    Conversion of html to ejs files, Connection to MongoDB in cloud, Create account, Login, cookies, email verification

--- app.js
+++ app.js
@@ -1,38 +1,185 @@
-//jshint esversion: 6
-
+//jshint esversion: 8
+require('dotenv').config();
 const express = require("express");
-const https = require("https");
 const bodyParser = require("body-parser");
-const request = require("request");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
-app.use(express.static("public"));
 
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: process.env.SESSION,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-brent:" + process.env.MONGODB + "@cluster0.1mr2f.mongodb.net/iBudget", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema ({
+  username: String,
+  password: String,
+  verified: Boolean(false)
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/login.html");
+  res.render("login");
 });
 
 app.post("/", function(req, res) {
   if (req.body.button === "login") {
-    res.sendFile(__dirname + "/failure.html");
+
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    req.login(user, function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+
+        passport.authenticate('local')(req, res, function() {
+          res.redirect("/budget");
+        });
+      }
+    });
   } else  {
-    res.sendFile(__dirname + "/create.html");
+    res.redirect("/create");
   }
+});
+
+app.post("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
 app.post("/failure", function(req, res) {
   res.redirect("/");
 });
 
+app.get("/budget", function(req, res) {
+  if (req.isAuthenticated()) {
+
+    res.render("budget", {username: req.user.username});
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.get("/create", function(req, res) {
+  res.render("create");
+});
+
 app.post("/create", function(req, res) {
-  res.sendFile(__dirname + "/failure.html");
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/create");
+    } else {
+      passport.authenticate('local')(req, res, function(){
+        emailAuth(req.body.username);
+
+        res.render("verify");
+      });
+    }
+  });
+});
+
+app.get("/verify", function(req, res) {
+  const token = req.query.id;
+  if (token) {
+    try {
+      jwt.verify(token, process.env.SECRET, (e, decoded) => {
+        if (e) {
+          console.log(e);
+          return res.sendStatus(403);
+        } else {
+          id = decoded.id;
+
+          User.updateOne({username: id}, {verified: true}, function(err) {
+            if (err) {
+              console.log(err);
+            } else {
+            }
+          });
+          //console.log("get for verify");
+          res.redirect("/budget");
+          //res.send("User verified!");
+          //update database with verification flag
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      return res.sendStatus(403);
+    }
+  } else {
+    return res.sendStatus(403);
+  }
 });
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000.");
 });
+
+function emailAuth(email) {
+  const date = new Date();
+  const mail = {
+    "id": email,
+    "created": date.toString()
+  };
+
+  const token_mail_verification = jwt.sign(mail, process.env.SECRET, {expiresIn: '1d'});
+  const url = "http://localhost:3000/" + "verify?id=" + token_mail_verification;
+
+  var transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "ibudgetauth",
+      pass: process.env.GMAIL
+    }
+  });
+
+  const mailOptions = {
+    from: "ibudgetauth@gmail.com",
+    to: email,
+    subject: "Please confirm your Email account to complete registration at iBudget",
+    html: "Hello,<br> Please click on the link to verify your email.<br><a href=" + url + ">Click here to verify</a>"
+  };
+
+  transporter.sendMail(mailOptions, function(err, res) {
+    if (err) {
+      console.log(err);
+      res.end("error");
+    } else {
+      console.log("Message sent: " + res.message);
+      res.end("sent");
+    }
+  });
+}
