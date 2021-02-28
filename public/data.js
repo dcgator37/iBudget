ae750ceb9c6b25556ff2abf74118c40757139713
Brent Baker <dcgator8@hotmail.com>
2021-02-28 15:54:30 -0500

    New chart filters on budget. Budget item sidebar is functional

--- public/data.js
+++ public/data.js
@@ -7,12 +7,93 @@ $(document).ready(function() {
   $.getScript("jquery.formatCurrency-1.4.0.js", function() {
 });
 
-  function updateChart() {
+$.getScript("Chart.js", function() {
+  console.log('chart loaded');
+});
 
-  }
+  //initialize global chart variable
+  var myChart;
 
+  //load budget data from db and create the chart with it
+  getChartData();
+
+  //format all the values on the site as currency
   $('.planned-label').toNumber().formatCurrency();
   $('.span-rem').toNumber().formatCurrency();
+
+
+  //listener when clicking off an item to hide item sidebar and re-display the chart
+  $(document).click(function(event) {
+    var $target = $(event.target);
+    if(!$target.closest('.item').length && !$target.closest('#monthPicker').length && !$target.closest('.Budget-List-Item').length  && !$target.closest('.Transactions-List').length) {
+      $('.item--selected').removeClass('item--selected');
+      $('#myChart').css("display", "block");
+      $('.Transactions-List').css("display", "none");
+      $('.Budget-List-Item').css("display", "none");
+    }
+  });
+
+  $(document).on('click', '.item', function() {
+
+    const index = $(this).parent().attr('data-cat');
+    const itemIndex = $(this).attr('data-item');
+    const categoryName = $(this).parent().attr('data-cat-name');
+    const itemName = $(this).children('.input-label').val();
+    const remaining = $(this).children('.span-rem').text();
+    const spent = $(this).children('.planned-label').attr('data-value') - $(this).children('.span-rem').attr('data-value');
+    const progressAmt = (spent / $(this).children('.planned-label').attr('data-value') * 100).toFixed(1);
+
+    //Remove the my-list--selected class from any elements that already have it
+    $('.item--selected').removeClass('item--selected');
+    //Add the my-list--selected class to the clicked element
+    $(this).addClass('item--selected');
+
+    console.log(progressAmt);
+
+    $('#catName').text(categoryName);
+    $('#itemName').text(itemName);
+    $('#remaining').text(remaining);
+    $('#spent').text(spent);
+    $('#spent').toNumber().formatCurrency();
+    $('.progress-bar').css('width', progressAmt + '%');
+    $('.progress-bar').attr('aria-valuenow', progressAmt);
+
+    $('#Transaction-Container').children('.Transactions-List-Row').slice(1).remove();
+
+    //ajax query to retrieve transactions from database
+    $.ajax({
+      url: '/getTransactions',
+      method: 'post',
+      dataType: 'json',
+      data: {
+        'index': index,
+        'itemIndex': itemIndex
+      },
+      success: function(res) {
+        if (res.msg == 'success') {
+          console.log(res.transactions);
+          updateTransactions(res.transactions);
+        } else {
+          alert('data did not get retrieved');
+        }
+      },
+      error: function(res) {
+        alert('server error occurred');
+      }
+    });
+
+    $('.Transactions-List').css("display", "flex");
+    $('.Budget-List-Item').css("display", "flex");
+
+    $('#myChart').css("display", "none");
+  });
+
+  $(document).on('click', '#closeBudgetListItem', function() {
+    $('.Transactions-List').css("display", "none");
+    $('.Budget-List-Item').css("display", "none");
+
+    $('#myChart').css("display", "block");
+  });
 
   // Add item button click event. Sends post request to server, sending the index of the category array you're adding an item to.
   // The server adds the new blank item to the database and responds with the index of the new item. When the server responds, run
@@ -88,6 +169,45 @@ $(document).ready(function() {
     });
   });
 
+  $(document).on('click', '#addTransaction', function() {
+    // var index = $(this).parent().attr('data-cat');
+    // var itemIndex = $(this).attr('data-item');
+
+    const itemIndex = $('.item--selected').attr('data-item');
+    const index = $('.item--selected').parent().attr('data-cat');
+    const el = $('.item--selected');
+    const amt = 25;
+    const merchant = 'test';
+
+    $.ajax({
+      url: '/addTransaction',
+      method: 'post',
+      dataType: 'json',
+      data: {
+        'type': 'Expense',
+        'amt': amt,
+        'date': new Date(),
+        'merchant': merchant,
+        'notes': 'test',
+        'index': index,
+        'itemIndex': itemIndex
+      },
+      success: function(res) {
+        if (res.msg == 'success') {
+          console.log('Success');
+          updateRemaining(el, res.sum);
+          updateBudgetListItem(el);
+          addTransactionList(merchant, amt);
+        } else {
+          alert('data did not get added');
+        }
+      },
+      error: function(res) {
+        alert('server error occurred');
+      }
+    });
+  });
+
 
   // Add category button click event. Sends post request to server.
   // The server adds the new blank category to the database and responds with the index of the new category. When the server responds, run
@@ -131,6 +251,7 @@ $(document).ready(function() {
       success: function(res) {
         if (res.msg == 'success') {
           el.remove();
+          updateChart(index, res.newCatSum);
         } else {
           alert('data did not get deleted');
         }
@@ -211,6 +332,8 @@ $(document).ready(function() {
         if (res.msg == 'success') {
           //function to update remaining value
           updateRemaining(el, res.sum);
+          updateChart(index, res.newCatSum);
+          updateBudgetListItem(el);
         } else {
           alert('data did not get edited');
         }
@@ -251,19 +374,22 @@ $(document).ready(function() {
   });
 
   //not used. will delete soon
-  function getData() {
+  function getChartData() {
     $.ajax({
-      url: '/testload',
+      url: '/testData',
       method: 'get',
       dataType: 'json',
-      success: (res) => {
+      success: function(res) {
         if (res.msg == 'success') {
-          //remove all the budget containers - does it remove everything inside too?
-          //loop through the budget object creating category containers and items
+          console.log(res.labels);
+          console.log(res.data);
+          createChart(res.labels, res.data);
+        } else {
+          alert('data did not get added');
         }
       },
-      error: (res) => {
-        aler('server error occurred');
+      error: function(res) {
+        alert('server error occurred');
       }
     });
   }
@@ -316,5 +442,102 @@ $(document).ready(function() {
     //format the span remaining as currency
     $(el).children('span').formatCurrency();
   }
+
+  function updateBudgetListItem(el) {
+    const remaining = $(el).children('.span-rem').text();
+    const spent = $(el).children('.planned-label').attr('data-value') - $(el).children('.span-rem').attr('data-value');
+    const progressAmt = (spent / $(el).children('.planned-label').attr('data-value') * 100).toFixed(1);
+
+    $('#remaining').text(remaining);
+    $('#spent').text(spent);
+    $('#spent').toNumber().formatCurrency();
+    $('.progress-bar').css('width', progressAmt + '%');
+    $('.progress-bar').attr('aria-valuenow', progressAmt);
+  }
+
+  function updateTransactions(transactions) {
+    const numTransactions = transactions.length;
+    const el = $('#Transaction-Container');
+    var htmlRow = '';
+
+    $('#numTransactions').text(numTransactions);
+
+    transactions.forEach((transaction) => {
+      htmlRow = "<div class='Transactions-List-Row'>" +
+                  "<span>Date</span>" +
+                  "<span>" + transaction.merchant + "</span>" +
+                  "<span class='transactionAmt'>" + transaction.amt + "</span>" +
+                "</div>";
+
+      $(el).append(htmlRow);
+
+    });
+
+    $('.transactionAmt').toNumber().formatCurrency();
+  }
+
+  function addTransactionList(merchant, amt) {
+    const el = $('#Transaction-Container');
+    var htmlRow = '';
+
+    htmlRow = "<div class='Transactions-List-Row'>" +
+                "<span>Date</span>" +
+                "<span>" + merchant + "</span>" +
+                "<span class='transactionAmt'>" + amt + "</span>" +
+              "</div>";
+
+    $(el).append(htmlRow);
+    $('.transactionAmt').toNumber().formatCurrency();
+  }
+
+  function createChart(labels, data) {
+    const chartData = {
+      datasets: [{
+          data: data,
+          backgroundColor: [
+                'rgba(255, 99, 132)',
+                'rgba(54, 162, 235)',
+                'rgba(255, 206, 86)',
+                'rgba(75, 192, 192)',
+                'rgba(153, 102, 255)',
+                'rgba(255, 159, 64)',
+                'rgba(244, 67, 54)',
+                'rgba(159, 39, 176)',
+                'rgba(63, 81, 81)',
+                'rgba(33, 150, 243)',
+                'rgba(0, 150, 136)',
+                'rgba(76, 175, 80)',
+                'rgba(255, 235, 59)',
+                'rgba(255, 152, 0)',
+                'rgba(255, 87, 34)'
+            ]
+      }],
+      labels : labels
+    };
+
+    const options = [];
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+    myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: chartData,
+      options: options
+    });
+  }
+
+  function updateChart(index, newCatSum) {
+    //var ctx = document.getElementById('myChart');
+    console.log(myChart);
+    myChart.config.data.datasets[0].data[index] = newCatSum;
+    myChart.update();
+  }
+
+  function addChartData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
 
 });
