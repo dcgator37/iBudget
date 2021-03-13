89fc81aca4a49fea1a166ab40cfe889fb0575ddc
Connor <cwalsh0831@gmail.com>
2021-03-13 16:13:42 -0500

    added left navigation menu, added code into budgetstyles.css and budget.ejs. added new file sidenav.js.

---
+++ public/sidenav.js
@@ -0,0 +1,10 @@
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("leftNav").style.width = "300px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("leftNav").style.width = "0";
+  }
